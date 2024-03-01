e269bd8cd0d66bfe36cc0f77eb17a02c0adc9394
Mr_Dalik <saryginvladislav2004@gmail.com>
2024-03-01 19:09:46 +0300

    +кнопка для добавления в рандом

--- wheel_of_fortune/lib/widget/circle.dart
+++ wheel_of_fortune/lib/widget/circle.dart
@@ -6,27 +6,30 @@ import 'package:flutter_dz/widget/sector_painter.dart';
 
 class Circle extends StatelessWidget {
   final List<String> names;
+  final double size;
 
-  const Circle({super.key, required this.names});
+  const Circle({super.key, required this.names, required this.size});
 
   @override
   Widget build(BuildContext context) {
     List<Widget> circle = [];
     for (int i = 1; i <= names.length; i++) {
-      circle.add(Transform.rotate(
-          angle: 2 * pi / names.length * (i-1),
+      circle.add(
+        Transform.rotate(
+          angle: 2 * pi / names.length * (i - 1),
           child: Stack(
             alignment: AlignmentDirectional.center,
             children: [
               CustomPaint(
                 painter: SectorPainter(
-                  color: names.length % 2 == 1
+                  color: Colors.cyan,
+                  /*names.length % 2 == 1
                       ? (i % 3 == 0
                           ? (Colors.white)
                           : (i % 3 == 1 ? (Colors.cyanAccent) : (Colors.red)))
-                      : (i % 2 == 1 ? Colors.cyanAccent : Colors.red),
+                      : (i % 2 == 1 ? Colors.cyanAccent : Colors.red),*/
                   angle: 2 * pi / names.length,
-                  radius: 140,
+                  radius: size / 2,
                 ),
               ),
               Transform.rotate(
@@ -34,19 +37,32 @@ class Circle extends StatelessWidget {
                 child: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Text(names[i - 1]),
-                    const SizedBox(
-                      width: 100,
-                    )
+                    const Spacer(),
+                    SizedBox(
+                        width: size / 5,
+                        child: Text(
+                          names[i - 1],
+                          style: TextStyle(
+                            fontSize: size / 30,
+                            fontWeight: FontWeight.w600,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          maxLines: 1,
+                        )),
+                    const Spacer(
+                      flex: 6,
+                    ),
                   ],
                 ),
-              )
+              ),
             ],
-          )));
+          ),
+        ),
+      );
     }
     return SizedBox(
-      width: 300,
-      height: 300,
+      width: size,
+      height: size,
       child: Stack(
         alignment: AlignmentDirectional.center,
         children: circle,
